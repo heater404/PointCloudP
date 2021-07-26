16f0593f883e56e6aa26f00e837b42b1e385a684
zhu daokuan <15549519416@163.com>
2021-07-26 20:36:34 +0800

    给固定像素信息功能增加防呆

--- Assets/Scripts/UIHelper/FixPixelInfo.cs
+++ Assets/Scripts/UIHelper/FixPixelInfo.cs
@@ -11,21 +11,53 @@ public class FixPixelInfo : MonoBehaviour
     // Use this for initialization
     public Text Info;
     private string xy;
+    Communication comm;
     void Start()
     {
-        xy = this.gameObject.GetComponent<InputField>().text = "320,240";
+        comm = GameObject.Find("Manager").GetComponent<Communication>();
         StartCoroutine(UpdateFixPixelInfo());
     }
 
     IEnumerator UpdateFixPixelInfo()
     {
+        xy = this.gameObject.GetComponent<InputField>().text = $"{comm.PixelWidth / 2},{comm.PixelHeight / 2}";
         while (true)
         {
+            xy = this.gameObject.GetComponent<InputField>().text;
             var xys = xy.Split(new char[] { ' ', ',', '，' }, System.StringSplitOptions.RemoveEmptyEntries);
             if (xys.Length == 2)
             {
-                var pixelSN = new Vector2Int(int.Parse(xys[0]), int.Parse(xys[1]));
-                Info.text = $"=> {DepthHelper.GetBufferData(pixelSN)}mm {GrayHelper.GetBufferData(pixelSN)}LSB";
+                int x, y;
+                if (int.TryParse(xys[0], out x))
+                {
+                    if (x > comm.PixelWidth - 1 || x < 0)
+                    {
+                        Info.text = $"OutOfRange:{x}";
+                    }
+                    else
+                    {
+                        if (int.TryParse(xys[1], out y))
+                        {
+                            if (y > comm.PixelHeight - 1 || y < 0)
+                            {
+                                Info.text = $"OutOfRange:{y}";
+                            }
+                            else
+                            {
+                                var pixelSN = new Vector2Int(x, y);
+                                Info.text = $"=> {DepthHelper.GetBufferData(pixelSN)}mm {GrayHelper.GetBufferData(pixelSN)}LSB";
+                            }
+                        }
+                        else
+                        {
+                            Info.text = $"formal error:{xys[1]}";
+                        }
+                    }
+                }
+                else
+                {
+                    Info.text = $"formal error:{xys[0]}";
+                }
             }
             else
             {
